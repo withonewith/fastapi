b496fbdcf8927fb3e42f353ea254cef7108c6f60
withonewith <sky30a@korea.ac.kr>
2024-03-15 18:56:08 +0900

    18:56

--- 27.py
+++ 27.py
@@ -1,17 +1,44 @@
-from fastapi import FastAPI
+from typing import Union
 from pydantic import BaseModel
+from fastapi import FastAPI
 
+class memoapp(BaseModel):
+    text: str
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-
+memo_storage: Union[memoapp, None] = None
 app = FastAPI()
 
+# 메모 create
+@app.post("/create/") 
+async def create_item(memo: memoapp):
+    # memo_storage 값 수정을 위해 전역 변수로 변경
+    global memo_storage
+    memo_storage = memo
+    return memo_storage
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+# 메모 read
+@app.get("/read/")
+async def read_item():
+    if memo_storage:
+        return memo_storage
+    return {"작성된 메모가 없습니다."}
+
+# 메모 update
+@app.put("/put/")
+async def update_item(memo_update: memoapp):
+    # memo_storage 값 수정을 위해 전역 변수로 변경
+    global memo_storage
+    if memo_storage:
+        memo_storage = memo_update
+        return memo_storage
+    return {"작성된 메모가 없습니다."} 
+
+# 메모 delete
+@app.delete("/delete/")
+async def delete_item():
+    # memo_storage 값 수정을 위해 전역 변수로 변경
+    global memo_storage
+    if memo_storage:
+        memo_storage = None
+        return memo_storage
+    return {"작성된 메모가 없습니다."} 
